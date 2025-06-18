dc52d7ee896c68ddfabd3d35b2efca1827d9124a
luciaspring <luciaspring@proton.me>
2025-06-18 19:57:56 +0100

    Updated InstagramPostCreator.tsx

--- app/components/InstagramPostCreator.tsx
+++ app/components/InstagramPostCreator.tsx
@@ -1794,17 +1794,7 @@ export default function InstagramPostCreator() {
               {/* --- PLAY / PAUSE OVAL (1/4 width) --- */}
               <Button
                 onClick={handlePlayClick}
-                className={`
-                  w-full
-                  h-full
-                  rounded-full flex items-center justify-center
-                  text-inherit
-                  transition-colors duration-300
-                  bg-[#E5E5E5] text-black hover:bg-[#9E9E9E]
-                  ${phase==='playing'
-                    ? 'bg-black text-white hover:bg-[#9E9E9E] hover:text-black'
-                    : ''}
-                `}
+                className="w-full h-full rounded-full flex items-center justify-center !bg-[#E5E5E5] text-black hover:!bg-[#CACACA]"
               >
                 {phase==='playing'
                   ? <span className="sf-icon text-xl">􀊅</span>
@@ -1815,14 +1805,14 @@ export default function InstagramPostCreator() {
               <div className="flex gap-2 w-full items-center">
                 <Button
                   onClick={() => setSettingsOpen(true)}
-                  className="flex-1 aspect-square bg-[#E5E5E5] text-black hover:bg-[#9E9E9E] rounded-none flex items-center justify-center text-inherit"
+                  className="flex-1 aspect-square !bg-[#E5E5E5] text-black hover:!bg-[#CACACA] rounded-none flex items-center justify-center text-inherit"
                 >
                   <span className="sf-icon text-xl">􀌆</span>
                 </Button>
 
                 <Button
                   onClick={exportVideo}
-                  className="flex-1 aspect-square bg-[#E5E5E5] text-black hover:bg-[#9E9E9E] rounded-none flex items-center justify-center text-inherit"
+                  className="flex-1 aspect-square !bg-[#E5E5E5] text-black hover:!bg-[#CACACA] rounded-none flex items-center justify-center text-inherit"
                 >
                   <span className="sf-icon text-xl">􀈂</span>
                 </Button>
