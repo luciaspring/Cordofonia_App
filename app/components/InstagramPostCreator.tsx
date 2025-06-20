7a7be9c3bf0f5cfcb313029a652c3c919eb0a8ee
luciaspring <luciaspring@proton.me>
2025-06-20 05:27:02 +0100

    Updated InstagramPostCreator.tsx

--- app/components/InstagramPostCreator.tsx
+++ app/components/InstagramPostCreator.tsx
@@ -575,7 +575,7 @@ export default function InstagramPostCreator() {
     ctx.font         = `${subPos.fontSize}px "${AFFAIRS}", sans-serif`
     ctx.fillStyle    = getContrastColor()
     ctx.textBaseline = 'alphabetic'
-    ctx.textAlign    = 'center'
+    ctx.textAlign    = 'left'
     ctx.fillText('Instrumento:', -subPos.width / 2, sBase)
     ctx.fillText(subtitle,       -subPos.width / 2, sBase + subPos.fontSize + 8)
     ctx.restore()
@@ -724,16 +724,23 @@ export default function InstagramPostCreator() {
     const dynSW     = sub1.width + (sub2.width - sub1.width) * scaleT
     const dynSH     = sub1.height + (sub2.height - sub1.height) * scaleT
 
-    ctx.save()
-    ctx.translate(sx + streX, sbaseline + streY)
-    ctx.rotate(srot)
-    ctx.font         = `${sFontSize}px "${AFFAIRS}", sans-serif`
-    ctx.fillStyle    = getContrastColor()
-    ctx.textBaseline = 'alphabetic'
-    ctx.textAlign    = 'left'
-    ctx.fillText('Instrumento:', 0, 0)
-    ctx.fillText(subtitle, 0, sFontSize + 8)
-    ctx.restore()
+    const sboxW    = dynSW
+    const sboxH    = dynSH
+    const stopY    = sbaseline - sub1.ascent
+    const scx      = sx + sboxW / 2
+    const scy      = stopY + sboxH / 2
+    const sBase    = sboxH / 2 - sub1.descent
+
+    ctx.save();
+    ctx.translate(scx + streX, scy + streY);
+    ctx.rotate(srot);
+    ctx.font         = `${sFontSize}px "${AFFAIRS}", sans-serif`;
+    ctx.fillStyle    = getContrastColor();
+    ctx.textBaseline = 'alphabetic';
+    ctx.textAlign    = 'left';
+    ctx.fillText('Instrumento:', -sboxW / 2, sBase);
+    ctx.fillText(subtitle,       -sboxW / 2, sBase + sFontSize + 8);
+    ctx.restore();
   }
 
   const drawBoundingBox = (ctx: CanvasRenderingContext2D, pos: TextPosition) => {
