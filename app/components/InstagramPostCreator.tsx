7587d9cb4fe6611ffb23e38a3e3672c5cd2f0968
luciaspring <luciaspring@proton.me>
2025-06-22 02:41:31 +0100

    Updated InstagramPostCreator.tsx

--- app/components/InstagramPostCreator.tsx
+++ app/components/InstagramPostCreator.tsx
@@ -567,23 +567,21 @@ export default function InstagramPostCreator() {
       ctx.restore()
     })
 
-    // subtitle  (baseline-left pivot, same as drawAnimatedText)
-    {
-      const tremX = (Math.random() - 0.5) * tremblingIntensity;
-      const tremY = (Math.random() - 0.5) * tremblingIntensity;
+    // subtitle
+    const tremXsub = (Math.random() - 0.5) * tremblingIntensity
+    const tremYsub = (Math.random() - 0.5) * tremblingIntensity
+    const { cx: scx, cy: scy, baselineOffset: sBase } = centerOf(subPos)
 
-      ctx.save();
-      ctx.translate(subPos.x + tremX, subPos.baseline + tremY); // pivot = baseline-left
-      ctx.rotate(subPos.rotation);
-      ctx.font         = `${subPos.fontSize}px "${AFFAIRS}", sans-serif`;
-      ctx.fillStyle    = getContrastColor();
-      ctx.textBaseline = 'alphabetic';
-      ctx.textAlign    = 'left';
-
-      ctx.fillText('Instrumento:', 0, 0);                // line 1
-      ctx.fillText(subtitle,        0, subPos.fontSize + 8); // line 2
-      ctx.restore();
-    }
+    ctx.save()
+    ctx.translate(scx + tremXsub, scy + tremYsub)
+    ctx.rotate(subPos.rotation)
+    ctx.font         = `${subPos.fontSize}px "${AFFAIRS}", sans-serif`
+    ctx.fillStyle    = getContrastColor()
+    ctx.textBaseline = 'alphabetic'
+    ctx.textAlign    = 'left'
+    ctx.fillText('Instrumento:', -subPos.width / 2, sBase)
+    ctx.fillText(subtitle,       -subPos.width / 2, sBase + subPos.fontSize + 8)
+    ctx.restore()
   }
 
   const drawRotatedText = (ctx: CanvasRenderingContext2D, pos: TextPosition, text: string) => {
@@ -2283,4 +2281,4 @@ const centerOf = (p: TextPosition) => {
     // baseline (cap-height of line 1) relative to the centre of the block
     baselineOffset: p.ascent - h / 2
   }
-} 
+}
